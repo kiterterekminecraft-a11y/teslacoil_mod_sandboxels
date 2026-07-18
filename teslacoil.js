3bcb8d8ebcc7a40f8f7d5174f3dc923b652b6182
kiterterekminecraft-a11y <kiterterekminecraft@gmail.com>
2026-07-19 00:07:49 +0200

    Update teslacoil.js

--- teslacoil.js
+++ teslacoil.js
@@ -1,28 +1,24 @@
-runAfterLoad(function() {
+alert("TESLA MOD START");
 
-    elements.tesla_coil = {
-        color: ["#555555", "#888888", "#bbbbbb"],
-        category: "machines",
-        state: "solid",
-        behavior: behaviors.WALL,
+elements.tesla_coil = {
+    color: "#888888",
+    category: "machines",
+    state: "solid",
+    behavior: behaviors.WALL,
 
-        conduct: 1,
-        hardness: 1,
+    conduct: 1,
+    hardness: 1,
 
-        desc: "Tesla Coil - creates electrical discharges.",
+    desc: "Tesla Coil - creates electric discharges.",
 
-        tick: function(pixel) {
+    tick: function(pixel) {
+        if (Math.random() < 0.1) {
+            let x = pixel.x + Math.floor(Math.random() * 7) - 3;
+            let y = pixel.y + Math.floor(Math.random() * 7) - 3;
 
-            if (Math.random() < 0.1) {
-
-                let x = pixel.x + Math.floor(Math.random()*7)-3;
-                let y = pixel.y + Math.floor(Math.random()*7)-3;
-
-                if (isEmpty(x,y)) {
-                    createPixel("electric", x, y);
-                }
+            if (isEmpty(x,y)) {
+                createPixel("electric", x, y);
             }
         }
-    };
-
-});
+    }
+};
