d9efd3aa8ae90fc217b0f38549fa8e0252099b39
kiterterekminecraft-a11y <kiterterekminecraft@gmail.com>
2026-07-19 15:44:57 +0200

    Update teslacoil.js

--- teslacoil.js
+++ teslacoil.js
@@ -1,8 +1,7 @@
-// Tesla Coil Mod for Sandboxels
+console.log("TESLA COIL MOD START");
 
 elements.tesla_coil = {
     name: "Tesla Coil",
-
     color: "#4b4b55",
 
     colorObject: {
@@ -12,60 +11,14 @@ elements.tesla_coil = {
     },
 
     behavior: behaviors.WALL,
-
     category: "special",
-
     state: "solid",
 
     density: 7800,
 
     insulate: true,
 
-    desc: "Generuje wyładowania elektryczne.",
-
-
-    tick: function(pixel) {
-
-        // szansa na wyładowanie
-        if (Math.random() < 0.15) {
-
-            var amount = Math.floor(Math.random() * 3) + 1;
-
-            for (var i = 0; i < amount; i++) {
-
-                var angle = Math.random() * Math.PI * 2;
-
-                var distance = Math.floor(Math.random() * 6) + 1;
-
-
-                var x = Math.round(
-                    pixel.x + Math.cos(angle) * distance
-                );
-
-                var y = Math.round(
-                    pixel.y + Math.sin(angle) * distance
-                );
-
-
-                if (!outOfBounds(x, y) && isEmpty(x, y)) {
-
-                    createPixel("electric", x, y);
-
-                }
-            }
-        }
-    }
+    desc: "Generuje wyładowania elektryczne."
 };
 
-
-// Dodanie do listy po załadowaniu
-runAfterLoad(function() {
-
-    // zabezpieczenie koloru
-    elements.tesla_coil.colorObject = {
-        r: 75,
-        g: 75,
-        b: 85
-    };
-
-});
+console.log("TESLA COIL MOD LOADED");
